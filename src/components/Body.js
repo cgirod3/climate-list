ccf2dc83bb4f8b001e10059aedb89d6c702fbfd7
cgirod3 <carlosegirod@gmail.com>
2021-08-15 15:39:05 -0400

    updated header styles

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 import CompanyTable from './CompanyTable';
 
+import '../styles/components/Body.scss';
+
 const Body = () => {
   return (
     <div className='Body'>
+      <span className='Body-goal'>
+        Climate Change is the most pressing issue facing the world today.
+        This page is intended to be a resource to connect those interested
+        in fixing the problem with companies actively working on solutions.
+      </span>
       <CompanyTable />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,14 @@ import '../styles/components/Header.scss';
 const Header = () => {
   return (
     <div className='Header'>
-      <h2>The GreenTech List</h2>
-      <h3 className='Header-quote'>"Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work."</h3>
-      <h3>- Steve Jobs</h3>
-      Climate Change is the most pressing issue facing the world today.
-      This page is intended to be a resource to connect those interested
-      in fixing the problem with companies actively working on solutions.
+      <h1 className='Header-text'>The GreenTech List</h1>
+      <div className='Header-quote'>
+        <div className='Header-quote-text'>
+          "Your work is going to fill a large part of your life,
+          and the only way to be truly satisfied is to do what you believe is great work."
+        </div>
+        - Steve Jobs
+      </div>
     </div>
   );
 };
