747d829bb1b0d0ffd037e25685217bd9723933b0
Peter Dolan <peter.dolan@reddit.com>
2021-08-14 20:17:44 -0700

    adds headers

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,12 @@ import '../styles/components/Header.css';
 const Header = () => {
   return (
     <div className='Header'>
-      <h2>Carlos and Peter's Green Tech List</h2>
+      <h2>The GreenTech List</h2>
+      <h3>"Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work."</h3>
+      <h3>- Steve Jobs</h3>
+      Climate Change is the most pressing issue facing the world today. 
+      This page is intended to be a resource to connect those interested
+      in fixing the problem with companies actively working on solutions. 
     </div>
   );
 };
